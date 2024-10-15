fc8b48e0836494030dc214835f1d8fce60a7ea29
Guilherme Rocha <guirocha1938@gmail.com>
2024-10-15 18:01:59 -0300

    feat: Adds cart visibility and management through redux toolkit.

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,22 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartItem.module.css';
+import { cartActions } from '../../store/cart-slice';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id } = props.item;
+  const dispatch = useDispatch();
+
+  const handleAddItem = () => {
+    dispatch(cartActions.addItem({
+      id,
+      title,
+      price,
+    }));
+  }
+
+  const handleRemoveItem = () => {
+    dispatch(cartActions.removeItem(id));
+  }
 
   return (
     <li className={classes.item}>
@@ -17,8 +32,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={handleRemoveItem}>-</button>
+          <button onClick={handleAddItem}>+</button>
         </div>
       </div>
     </li>
